1632dfc205e88e7fed8831a475328468520b9767
Kevin Karneboge <kkarneboge@gmail.com>
2022-08-15 13:59:19 +0000

    completed prep javascript for loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,6 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) { console.log(i); }
+
+for (var i = 0; i < 10; i++) { console.log(i * 2); }
+
+for (var i = 0; i < 100; i++) { console.log('Time until explosion ' + (100 - i)); }
